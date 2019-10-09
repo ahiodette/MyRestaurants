63472901b1441ee2048f794eef2141392ce8ab0f
ahiodette <ahiode6@gmail.com>
2019-10-09 15:13:10 +0200

    ButterKnife implementation

--- app/src/main/java/com/moringaschool/myrestaurants/MainActivity.java
+++ app/src/main/java/com/moringaschool/myrestaurants/MainActivity.java
@@ -1,32 +1,42 @@
 package com.moringaschool.myrestaurants;
 
 
-import android.content.Intent;
-
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.util.Log;
+
+
+import android.content.Intent;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.lang.reflect.Type;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
-    private Button mFindRestaurantsButton;
-    private EditText mLocationEditText;
+    private static final String TAG = MainActivity.class.getSimpleName();
+    @BindView(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
+    @BindView(R.id.locationEditText) EditText mLocationEditText;
+    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
-        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+        ButterKnife.bind(this);
 
-        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+        Typeface caviarFont = Typeface.createFromAsset(getAssets(), "fonts/CaviarDreams.ttf");
+        mAppNameTextView.setTypeface(caviarFont);
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
-                String location = mLocationEditText.getText().toString();
+            public void onClick(View v){
                 Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                String location = mLocationEditText.getText().toString();
                 intent.putExtra("location", location);
                 startActivity(intent);
             }
--- app/src/main/java/com/moringaschool/myrestaurants/RestaurantsActivity.java
+++ app/src/main/java/com/moringaschool/myrestaurants/RestaurantsActivity.java
@@ -2,25 +2,46 @@ package com.moringaschool.myrestaurants;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.moringaschool.myrestaurants.R;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class RestaurantsActivity extends AppCompatActivity {
-    public static final String TAG = RestaurantsActivity.class.getSimpleName();
-    private TextView mLocationTextView;
+    @BindView(R.id.locationTextView) TextView mLocationTextView;
+    @BindView(R.id.listView) ListView mListView;
+    private String[] restaurants = new String[] {"Mi Mero Mole", "Mother's Bistro",
+            "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
+            "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
+            "Lardo", "Portland City Grill", "Fat Head's Brewery",
+            "Chipotle", "Subway"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
-
-        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+        ButterKnife.bind(this);
 
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants);
+        mListView.setAdapter(adapter);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l){
+                String restaurant = ((TextView)view).getText().toString();
+                Toast.makeText(RestaurantsActivity.this, restaurant, Toast.LENGTH_LONG).show();
+            }
+        });
         mLocationTextView.setText("Here are all the restaurants near: " + location);
     }
+
+
 }
